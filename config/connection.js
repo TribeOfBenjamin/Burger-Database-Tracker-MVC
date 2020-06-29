631a7937a12d19cf13ba36d17fb2f44cdc50639f
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-06-29 16:07:22 -0500

    added Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -3,26 +3,26 @@ const mysql = require("mysql");
 
 const connection
 
-= mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "NewYork1991",
-  database: "burgers_db"
-});
+// = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "NewYork1991",
+//   database: "burgers_db"
+// });
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "NewYork1991",
-//     database: "burgers_db"
-//   });
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "NewYork1991",
+    database: "burgers_db"
+  });
   
-// }
+}
 
 
 // Make connection.
