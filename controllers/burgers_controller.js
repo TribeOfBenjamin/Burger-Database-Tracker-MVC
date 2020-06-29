bc24a9adfcb07c2a88ae8dded8fff4a9a684db71
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-06-29 09:21:28 -0500

    added put function for controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,6 +19,20 @@ router.post("/api/burgers", function(req, res) {
     });
 });
 
+router.put("/api/burgers/:id", function(req, res) {
+    let conditionVal = req.params.id;
+
+    console.log("condition value", conditionVal);
+
+    burgers.updateOne("burger_name", req.body.burger_name, "id", conditionVal, function(result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          };
+    });
+});
 
 // For insert I only have the burger name; devoured truthy statement NOT included;
 // USE ARRAYS (LIKE IN CATS.JS) IF ADD DEVOURED!
