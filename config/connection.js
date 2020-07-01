ac0aa5eb9d61f294cf31789b95e9bcaa9f7b6db8
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-06-30 22:12:13 -0500

    db

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,31 @@
 const mysql = require("mysql");
 
-// const connection
-// = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "NewYork1991",
-//   database: "burgers_db"
-// });
-
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "NewYork1991",
-//     database: "burgers_db"
-//   });
-// };
-
-
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "NewYork1991",
+  database: "burgers_db"
+});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "NewYork1991",
+    database: "burgers_db"
   });
+};
+
+
+// const connection = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+//   });
 
 
 connection.connect(function(err) {
